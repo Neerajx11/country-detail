f1e2b37bacfee69f9c549a6e877a4ebc7b538b92
Neeraj Maurya <61646801+Neerajx11@users.noreply.github.com>
2022-03-30 21:01:34 +0530

    Add text to Loader

--- src/Components/Loader.js
+++ src/Components/Loader.js
@@ -2,7 +2,7 @@ import React from "react";
 import "../Css/Loader.css";
 
 const Loader = () => {
-  return <div className="loader"></div>;
+  return <div className="loader">Loading...</div>;
 };
 
 export default Loader;
